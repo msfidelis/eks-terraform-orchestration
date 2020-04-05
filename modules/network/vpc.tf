f56b0567ba7161f92d7cf62b95607a66a483ed99
Matheus Fidelis <matheus@nanoshots.com.br>
2020-04-05 12:56:56 -0300

    nat gateway

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -1,12 +1,11 @@
 resource "aws_vpc" "cluster_vpc" {
-  cidr_block            = "10.0.0.0/16"
-  enable_dns_support    = true
-  enable_dns_hostnames  = true
-
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = map(
-     "Name", "${var.cluster_name}-vpc",
-     "kubernetes.io/cluster/${var.cluster_name}", "shared"
-    )
+    "Name", "${var.cluster_name}-vpc",
+    "kubernetes.io/cluster/${var.cluster_name}", "shared"
+  )
 
 }
